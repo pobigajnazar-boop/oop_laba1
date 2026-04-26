d980942215dadf980f63a80378d40980f5c586e8
Nazar <pobigajnazar@gmail.com>
2026-04-26 13:57:45 +0300

    Fix repository structure and add tests

---
+++ oop laba1/TestProject1/UnitTest1.cs
@@ -0,0 +1,40 @@
+﻿using Xunit;
+
+namespace TestProject2
+{
+    public class MatrixTests
+    {
+        [Fact]
+        public void MultiplyMatrix_ShouldMultiplyEachElementByConstant()
+        {
+            char[,] inputMatrix = {
+                { (char)2, (char)3 },
+                { (char)4, (char)5 }
+            };
+            char constant = (char)3;
+            char[,] expectedMatrix = {
+                { (char)6, (char)9 },
+                { (char)12, (char)15 }
+            };
+
+            char[,] actualMatrix = Lab1.MultiplyMatrix(inputMatrix, constant);
+
+            Assert.Equal(expectedMatrix, actualMatrix);
+        }
+
+        [Fact]
+        public void FindMinInColumns_ShouldReturnMinimumsForCorrectColumns()
+        {
+            char[,] matrix = {
+                { (char)20, (char)40, (char)60 },
+                { (char)30, (char)10, (char)50 },
+                { (char)16, (char)24, (char)36 }
+            };
+            char[] expectedMins = { (char)16, (char)10, (char)36 };
+
+            char[] actualMins = Lab1.FindMinInColumns(matrix);
+
+            Assert.Equal(expectedMins, actualMins);
+        }
+    }
+}
---
+++ oop laba1/oop laba1/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+
+public class Lab1
+{
+    public static char[,] MultiplyMatrix(char[,] matrix, char a)
+    {
+        int rows = matrix.GetLength(0);
+        int cols = matrix.GetLength(1);
+        char[,] result = new char[rows, cols];
+
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < cols; j++)
+            {
+                result[i, j] = (char)(matrix[i, j] * a);
+            }
+        }
+        return result;
+    }
+    
+    public static char[] FindMinInColumns(char[,] matrix)
+    {
+        int rows = matrix.GetLength(0);
+        int cols = matrix.GetLength(1);
+        char[] mins = new char[cols];
+
+        for (int j = 0; j < cols; j++)
+        {
+            char minInCol = matrix[0, j];
+            for (int i = 1; i < rows; i++)
+            {
+                if (matrix[i, j] < minInCol)
+                {
+                    minInCol = matrix[i, j];
+                }
+            }
+            mins[j] = minInCol;
+        }
+        return mins;
+    }
+
+    public static void Main(string[] args)
+    {
+        Console.OutputEncoding = System.Text.Encoding.UTF8;
+        try
+        {
+            char a = (char)2;
+            char[,] b = {
+                { (char)10, (char)20, (char)30 },
+                { (char)15, (char)5,  (char)25 },
+                { (char)8,  (char)12, (char)18 }
+            };
+
+            char[,] C = MultiplyMatrix(b, a);
+
+            Console.WriteLine("Результуюча матриця C:");
+            for (int i = 0; i < C.GetLength(0); i++)
+            {
+                for (int j = 0; j < C.GetLength(1); j++)
+                {
+                    Console.Write((int)C[i, j] + "\t");
+                }
+                Console.WriteLine();
+            }
+
+            char[] mins = FindMinInColumns(C);
+            long sumOfMins = 0;
+
+            Console.WriteLine("\nНайменші елементи по стовпцях:");
+            for (int j = 0; j < mins.Length; j++)
+            {
+                Console.WriteLine($"Стовпець {j + 1}: {(int)mins[j]}");
+                sumOfMins += mins[j];
+            }
+            Console.WriteLine($"\nЗагальна сума найменших елементів: {sumOfMins}");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Помилка: {ex.Message}");
+        }
+    }
+}
